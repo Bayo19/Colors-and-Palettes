52e3c312c109eab62b937e87a6ae99820019cfba
Bayo Ade <bayo.300@gmail.com>
2020-11-02 13:53:54 +0000

    Trying to add events to dynamic html

--- js/saved.js
+++ js/saved.js
@@ -47,41 +47,47 @@
             root.innerHTML = `${
         palettes.map(function(palette){
     
-            return `<div class="saved-palette">
-                <div class="saved-palette-child-one" style="background:${'#' + palette[0]}"></div>
-                <div class="saved-palette-child-two" style="background:${'#' + palette[1]}"></div>
-                <div class="saved-palette-child-three" style="background:${'#' + palette[2]}"></div>
-                <div class="saved-palette-child-four" style="background:${'#' + palette[3]}"></div>
-                <div class="saved-palette-child-five" style="background:${'#' + palette[4]}"></div>
+            return `<div id="saved-palette">
+                <div class="saved-palette-child-one" style="background:${'#' + palette[0]}"><p>${palette[0]}</p></div>
+                <div class="saved-palette-child-two" style="background:${'#' + palette[1]}"><p>${palette[1]}</p></div>
+                <div class="saved-palette-child-three" style="background:${'#' + palette[2]}"><p>${palette[2]}</p></div>
+                <div class="saved-palette-child-four" style="background:${'#' + palette[3]}"><p>${palette[3]}</p></div>
+                <div class="saved-palette-child-five" style="background:${'#' + palette[4]}"><p>${palette[4]}</p></div>
                 </div>`
             }).join('')
         }`
     }       
     
-  
-    document.addEventListener('dblclick', function(e){
-
-        let res = e.target.style.backgroundColor.replace(/[a-z\(\)]/g, '').split(',')
-        let [r, g, b] = res
-        let hsp = Math.sqrt(
-            0.299 * (r * r) +
-            0.587 * (g * g) +
-            0.114 * (b * b)
-        )
-        if (hsp >= 128) {
-            e.target.style.color = 'black'
-        } else {
-        e.target.style.color = 'white'
-        }
-
-        if(e.target && e.target.className == 'saved-palette-child-one'|| e.target.className == 'saved-palette-child-two'|| e.target.className == 'saved-palette-child-three' || e.target.className == 'saved-palette-child-four' || e.target.className == 'saved-palette-child-five') {
-            console.log(e.target.style.backgroundColor)
-            navigator.clipboard.writeText(toHex(e.target.style.backgroundColor))
-            e.target.textContent = 'copied'
-            setTimeout(function() {
-                e.target.textContent = ''
-            }, 1200)
+    document.addEventListener('load', function(e){
+        if(e.target && e.target.id == 'saved-palette') {
+            console.log('hello')
         }
     })
+  
+    // document.addEventListener('dblclick', function(e){
 
+    //     let res = e.target.style.backgroundColor.replace(/[a-z\(\)]/g, '').split(',')
+    //     let [r, g, b] = res
+    //     let hsp = Math.sqrt(
+    //         0.299 * (r * r) +
+    //         0.587 * (g * g) +
+    //         0.114 * (b * b)
+    //     )
+    //     if (hsp >= 128) {
+    //         e.target.style.color = 'black'
+    //     } else {
+    //     e.target.style.color = 'white'
+    //     }
+
+    //     if(e.target && e.target.className == 'saved-palette-child-one'|| e.target.className == 'saved-palette-child-two'|| e.target.className == 'saved-palette-child-three' || e.target.className == 'saved-palette-child-four' || e.target.className == 'saved-palette-child-five') {
+    //         console.log(e.target.style.backgroundColor)
+    //         navigator.clipboard.writeText(toHex(e.target.style.backgroundColor))
+    //         e.target.textContent = 'copied'
+    //         setTimeout(function() {
+    //             e.target.textContent = ''
+    //         }, 1200)
+    //     }
+    // })
+
+    
 })()
